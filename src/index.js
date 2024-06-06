8d32eaa0c4eb7adf37aa443bfc0820d57d600329
Kauane Santos <kauanesantos.coding@gmail.com>
2024-06-06 13:19:34 -0300

    refactor: create Page folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './main';
+import Main from './Page/main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
